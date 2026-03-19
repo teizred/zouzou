bb5771d1c82bbcad0393114830688f1f3cc31a07
teizred <91955720+teizred@users.noreply.github.com>
2026-03-19 15:23:19 +0100

    refactor: bug fix blank screen

--- db/index.ts
+++ db/index.ts
@@ -2,8 +2,18 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-const client = postgres(process.env.DATABASE_URL!, { 
-    prepare: false, 
-    ssl: 'require' 
+// Singleton connection to prevent "MaxClientsInSessionMode" errors in dev/Vercel
+const globalForPostgres = globalThis as unknown as {
+  postgres: postgres.Sql | undefined
+}
+
+const connectionString = process.env.DATABASE_URL!
+
+const client = globalForPostgres.postgres || postgres(connectionString, { 
+    prepare: false,
+    ssl: 'require'
 })
+
+if (process.env.NODE_ENV !== 'production') globalForPostgres.postgres = client
+
 export const db = drizzle(client, { schema })
